332f9b3db04986e05cb4f24e6b0c59100c4af921
Anders Johannsen <anders@johannsen.com>
2015-09-20 23:00:37 +0200

    conversion scripts

---
+++ conversion/convert.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+OUT=../dimsum16.train
+
+python sst_to_dimsum.py original/lowlands.UPOS2.tsv lowlands > $OUT
+python sst_to_dimsum.py original/ritter.UPOS2.tsv ritter >> $OUT
+python streusel_to_dimsum.py original/streusle.upos.tags >> $OUT
